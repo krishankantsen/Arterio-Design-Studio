337fd818ff8bdf4593c627cd7d6d1ed18cbdf29e
Krishan Kant Sen <75468514+krishankantsen@users.noreply.github.com>
2026-02-25 04:31:19 +0000

    sdfgh

--- app/page.tsx
+++ app/page.tsx
@@ -8,11 +8,9 @@ export default function Home() {
   return (
     <>
       <Hero images={[
-    'https://old-crimson-nyt2t23tht.edgeone.app/file_00000000a5dc71faa6e5f9839c54df83.png',
-    'https://blog.novatr.com/hubfs/3D_model.jpg',
-    'https://dpkverma.in/wp-content/uploads/2022/08/6-1-scaled.jpg',
-    'https://images.pexels.com/photos/5583620/pexels-photo-5583620.jpeg',
-  ]}/>
+        'https://old-crimson-nyt2t23tht.edgeone.app/file_00000000a5dc71faa6e5f9839c54df83.png',
+        'https://blog.novatr.com/hubfs/3D_model.jpg'
+      ]} />
       <Portfolio />
       <Services />
       <About />
