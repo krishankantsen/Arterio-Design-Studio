05ec5376e9f48c5b266629938bb80983aa3155b2
Krishan Kant Sen <75468514+krishankantsen@users.noreply.github.com>
2026-02-25 18:02:49 +0000

    dfg

--- components/Portfolio.tsx
+++ components/Portfolio.tsx
@@ -82,7 +82,7 @@ export default function Portfolio() {
             <h3 className="text-2xl font-semibold mb-4 text-center">
               Portfolio Gallery
             </h3>
-            <div className="grid grid-cols-2 sm:grid-cols-3 gap-4">
+            <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
               {galleryImages?.map((img,index) => (
                 <div key={index} className="aspect-w-4 aspect-h-3 relative">
                   <img
